efd63bcb39af52266f667b81b0ced3405ad9c549
bazrad <91244573+bazrad@users.noreply.github.com>
2025-05-14 17:47:31 +0800

    Засвар оруулав footer хэсэгт

--- src/page/App/index.jsx
+++ src/page/App/index.jsx
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import "./style.module.css";
 import Sidebar from "../../components/Sidebar";
 import Products from "../Products";
+import Footer from "../../components/Footer";
+
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
 
@@ -12,12 +14,13 @@ function App() {
   };
   return (
     <>
-      <div className="w-full h-[20000px] flex top-0 left-0 ">
+      <div className="w-full h-[20000px] top-0 left-0 ">
         <Navbar toggleSidebar={toggleSidebar} />
         <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
         <Routes>
           <Route path="products" element={<Products />} />
         </Routes>
+        <Footer/>
       </div>
     </>
   );
