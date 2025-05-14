1755ea28865031d58f2fc8265ad861bf2e0bab56
Zolbayar <zzolbaya@gmail.com>
2025-05-14 11:44:54 +0800

    Vite uusgelee

---
+++ zevTabs/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-white w-screen">Test</div>
+    </>
+  );
+}
+
+export default App;
